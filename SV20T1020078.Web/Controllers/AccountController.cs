9cd317814a7822a203fff7aeb1863a6326cebc8f
ADMIN <phucdaica549@gmail.com>
2024-02-21 21:04:50 +0700

    ss

---
+++ SV20T1020078.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SV20T1020078.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+        public IActionResult Login()
+        {
+            if(Request.Method == "POST")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
